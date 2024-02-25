78ff4e2de7726a4a72bb1261b8aa0ad3e0e942b4
Ubuntu <ubuntu@ip-10-0-12-246.ap-northeast-1.compute.internal>
2024-02-25 14:19:54 +0000

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = props => {
+	const [name, setName] = useState(props.name)
+	const [price, setPrice] = useState(props.price)
+	const reset = () => {
+		setPrice(props.price)
+		setName(props.name)
+	}
+	return (
+		<>
+			<p>現在の{name}は、{price}円です。</p>
+			<button onClick={()=>setPrice(price + 1)}>+1</button>
+			<button onClick={()=>setPrice(price - 1)}>-1</button>
+			<button onClick={reset}>リセット
+</button>
+			<input value={name} onChange={e => setName(e.target.value)}/>
+		</>
+  )
 }
-
-export default App;
+App.defaultProps = {
+	name: '',
+	price: 1000
+}
+export default App
